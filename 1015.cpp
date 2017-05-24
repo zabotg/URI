bd1909837e96640959f57547ba6163b8243e29c4
Guilherme Zabot <zabot.gui@gmail.com>
2017-05-24 20:05:22 -0300

    Uri 1023

--- 1015.cpp
+++ 1015.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-class Point 
-{
+class Point {
 private:
 	double x1, x2, y1, y2;
 public:
@@ -12,21 +11,18 @@ public:
 	double dist();
 };
 
-Point::Point(double x1, double y1, double x2, double y2)
-{
+Point::Point(double x1, double y1, double x2, double y2){
 	this->x1 = x1;
 	this->y1 = y1;
 	this->x2 = x2;
 	this->y2 = y2;
 }
 
-double Point::dist()
-{
+double Point::dist(){
 	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
-int main()
-{
+int main() {
 	double x1, x2, y1, y2;
 	
 	cin >> x1 >> y1 >> x2 >> y2;
